a1afe67dc8a6cca5b7146eb5d73d3da7dc05184f
mariap9 <55482656+mariap9@users.noreply.github.com>
2019-10-16 09:45:46 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,66 @@
-function preload(){
-  // put preload code here
+var illus;
+let song, analyzer;
+
+// load jpg. and mp3 files
+function preload() {
+  illus = loadImage("./assets/home2.jpg");
+  song = loadSound("./assets/opera.mp3");
+}
+
+// function for mouse interaction
+function togglePlay() {
+  if (song.isPlaying() == false) {
+    song.play();
+  } else {
+    song.stop();
+  }
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+
+  // allow any part of the window as a play/stop toggle
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.mouseClicked(togglePlay);
+  song.play();
+
+  // link the data of the amplitude with the file song
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(song);
 }
 
+
 function draw() {
-  // put drawing code here
+  // call the image file as background
+  image(illus, windowWidth / 2, (windowHeight / 2) - 50, illus.width / 2, illus.height / 2);
+  let rms = analyzer.getLevel();
+  fill(200, 200, 200, 50);
+  stroke(0);
+  strokeWeight(1.5);
+  ellipse((width / 2) - 32, (height / 2) + 55, 15 + rms * 250, 3 + rms * 700);
+
+
+  var titlehome = "· A home solo ·";
+  drawingContext.font = "normal 13px Roboto Slab";
+  drawingContext.textAlign = "center";
+  fill(0);
+  noStroke()
+  text(titlehome, (width / 2) - 18, (height / 2) + 165);
+
+  var int = "click to listen";
+  drawingContext.font = "normal 8px Roboto Slab";
+  drawingContext.textAlign = "center";
+  fill(220);
+  noStroke()
+  text(int, (width / 2) - 18, (height / 2) + 180);
+
+  fill(220);
+  noStroke();
+  translate(windowWidth / 2, windowHeight / 2);
+  scale(1);
+  triangle(-32, 145, -20, 130, -7, 145);
+  scale(0.5);
+
 }
